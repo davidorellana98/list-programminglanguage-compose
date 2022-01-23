a8b89e96571b5897083497a4f5d2de7566039ae8
davidorellana <davidorellana_98@hotmail.com>
2022-01-23 18:24:10 -0500

    Inicialización del proyecto

---
+++ app/src/main/java/com/davidorellana/listprogramminglanguagecompose/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.davidorellana.listprogramminglanguagecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.davidorellana.listprogramminglanguagecompose.ui.theme.ListProgrammingLanguageComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListProgrammingLanguageComposeTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
