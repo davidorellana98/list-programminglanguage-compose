76aedcce01e3811e4d5e0b16b423a2bd5a1662aa
davidorellana <davidorellana_98@hotmail.com>
2022-01-23 19:29:16 -0500

    Creación Listado de Lenguajes de programación

---
+++ app/src/main/java/com/davidorellana/listprogramminglanguagecompose/ProgrammingLanguageListProvider.kt
@@ -0,0 +1,231 @@
+package com.davidorellana.listprogramminglanguagecompose
+
+object ProgrammingLanguageListProvider {
+
+    val programmingLanguageList = listOf(
+        ProgrammingLanguage(
+            nameLanguage = "Java",
+            author = "Sun Microsystems",
+            yearCreation = 1995,
+            paradigm = "Programación orientada a objetos, imperativo y funcional",
+            website = "https://www.java.com/es/",
+            photoLogo = "https://ppss.kr/wp-content/uploads/2020/03/03-4-768x432.jpg"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Swift",
+            author = "Apple",
+            yearCreation = 2014,
+            paradigm = "Multiparadigma (orientado a prototipos, objetos, funcional e imperativo)",
+            website = "https://www.swift.org/",
+            photoLogo = "https://iphoneros.com/wp-content/uploads/2015/12/logodeswift.jpg"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Kotlin",
+            author = "JetBrains",
+            yearCreation = 2016,
+            paradigm = "Programación orientado a objetos y funcional",
+            website = "https://kotlinlang.org/",
+            photoLogo = "https://avatars.githubusercontent.com/u/1446536?v=4&s=160"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Haskell",
+            author = "Universidad de Yale y Universidad de Glasgow",
+            yearCreation = 1990,
+            paradigm = "Programación funcional",
+            website = "https://www.haskell.org/",
+            photoLogo = "https://chrisconlan.com/wp-content/uploads/2018/06/haskell_logo_2.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "JavaScript",
+            author = "Nestscape Communications",
+            yearCreation = 1995,
+            paradigm = "Multiparadigma (basada en prototipos, funcional, imperativo e interpretado)",
+            website = "https://es.wikipedia.org/wiki/JavaScript",
+            photoLogo = "https://th.bing.com/th/id/OIP.eaaXHFu1rDxq5EeVajUamAHaHa?pid=ImgDet&w=1200&h=1200&rs=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Python",
+            author = "Guido Van Rossum",
+            yearCreation = 1991,
+            paradigm = "Multiparadigma (orientado a objetos, imperativo, reflexivo y funcional)",
+            website = "https://www.python.org/",
+            photoLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Go",
+            author = "Robert Griesemer, Rob Pike y Ken Thompson",
+            yearCreation = 2009,
+            paradigm = "Compilado concurrente, imperativo y estructurado",
+            website = "https://go.dev/",
+            photoLogo = "https://th.bing.com/th/id/R.83643173771eb9b1bda2ba589f92cb34?rik=%2fulflZWx3hGFog&riu=http%3a%2f%2fwww.vozidea.com%2fwp-content%2fuploads%2f2016%2f05%2fgo-golang-logo.jpg&ehk=svdc2z%2b7XOgfkeWvI%2bTc2Yj3deGNq%2fdhId18Sa0aW00%3d&risl=&pid=ImgRaw&r=0"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "C Sharp (C#)",
+            author = "Microsoft",
+            yearCreation = 2000,
+            paradigm = "Multiparadigma (estructurado, imperativo, orientado a objetos, dirigido por eventos, funcional y reflexivo)",
+            website = "https://docs.microsoft.com/es-es/dotnet/csharp/",
+            photoLogo = "https://tistory2.daumcdn.net/tistory/4064248/attach/bbcc0a155bd240f2aa5adb788eb21e92"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "C++",
+            author = "Bjarne Stroustrup",
+            yearCreation = 1983,
+            paradigm = "Multiparadigma (estructurado, imperativo y genérica)",
+            website = "https://es.wikipedia.org/wiki/C++",
+            photoLogo = "https://th.bing.com/th/id/R.b49ecd507d22da897986f595e4a17aec?rik=yWkHDNjbEnUBow&riu=http%3a%2f%2fwww.innovarsi.it%2fwp-content%2fuploads%2f2016%2f10%2fprogrammazione-cpp.jpg&ehk=YneF%2bEt89zIecj8JwH1iCS%2fLw4r2rl5Uhl9q%2bfWrOP4%3d&risl=&pid=ImgRaw&r=0"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "C",
+            author = "Dennis Ritchie",
+            yearCreation = 1972,
+            paradigm = "Programación imperativa (procedural) y estructurado",
+            website = "https://es.wikipedia.org/wiki/C_(lenguaje_de_programaci%C3%B3n)",
+            photoLogo = "https://disenowebakus.net/imagenes/materias/lenguaje-de-programacion-c.jpg"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Dart",
+            author = "Google",
+            yearCreation = 2011,
+            paradigm = "Programación orientado a objetos y funcional",
+            website = "https://dart.dev/",
+            photoLogo = "https://dwglogo.com/wp-content/uploads/2018/03/Dart_logo.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Scala",
+            author = "Martin Odersky",
+            yearCreation = 2003,
+            paradigm = "Programación orientado a objetos y funcional",
+            website = "https://www.scala-lang.org/",
+            photoLogo = "https://dwglogo.com/wp-content/uploads/2017/09/1300px-Scala_logo.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Fortran",
+            author = "Jhon Backus",
+            yearCreation = 1957,
+            paradigm = "Programación procedimental e imperativo",
+            website = "https://fortran-lang.org/",
+            photoLogo = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Fortran_logo.svg/80px-Fortran_logo.svg.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "PHP",
+            author = "Rasmus Lerdorf",
+            yearCreation = 1995,
+            paradigm = "Multiparadigma (orientado a objetos, imperativo y funcional)",
+            website = "https://www.php.net/",
+            photoLogo = "https://th.bing.com/th/id/OIP.PmODBAVvOJ8JJp-L1yYiNgHaE8?pid=ImgDet&w=480&h=320&rs=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Rust",
+            author = "Graydon Hoare",
+            yearCreation = 2010,
+            paradigm = "Multiparadigma (orientado a objetos, imperativo y funcional)",
+            website = "https://www.rust-lang.org/",
+            photoLogo = "https://s3.amazonaws.com/s3.timetoast.com/public/uploads/photos/12213586/dims.jpg"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "TypeScript",
+            author = "Microsoft",
+            yearCreation = 2012,
+            paradigm = "Multiparadigma (estructurado, orientado a objetos, imperativo, script y genérico)",
+            website = "https://www.typescriptlang.org/",
+            photoLogo = "https://blog.jeremylikness.com/blog/2019-03-05_typescript-for-javascript-developers-by-refactoring-part-1-of-2/images/1.jpeg"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "R",
+            author = "Ross Ihaka y Robert Gentleman",
+            yearCreation = 1993,
+            paradigm = "Multiparadigma (funcional, vectorial, imperativo, procedural y orientado a objetos)",
+            website = "https://www.r-project.org/",
+            photoLogo = "https://th.bing.com/th/id/R.ef3ab4eded24bc55c507573942df7273?rik=9iN5xxeR0mzSbA&riu=http%3a%2f%2ffiles.idg.co.kr%2fciokr%2fimages%2fr.png&ehk=2CqHC9Iuq5KDPfZcNtuPwWlXma4V8iZnUhVSRRJLtWk%3d&risl=&pid=ImgRaw&r=0"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Ruby",
+            author = "Yukihiro Matsumoto",
+            yearCreation = 1995,
+            paradigm = "Multiparadigma (orientado a objetos y reflexivo)",
+            website = "https://www.ruby-lang.org/es/",
+            photoLogo = "https://1.bp.blogspot.com/-l91oSiV5rX0/XhRann1bPuI/AAAAAAAACbk/ChrygVgo8jg9Xq1wQdFongv2pOO0pwQpwCLcBGAsYHQ/s1600/ruby.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Assembly",
+            author = "s/n",
+            yearCreation = 1949,
+            paradigm = "Imperativo y desestructurado",
+            website = "https://es.wikipedia.org/wiki/Lenguaje_ensamblador",
+            photoLogo = "https://th.bing.com/th/id/OIP.v5m2HW3FI3RbopnK8Cs6HgAAAA?pid=ImgDet&rs=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "VB .NET",
+            author = "Microsoft",
+            yearCreation = 2001,
+            paradigm = "Multiparadigma (estructurado, imperativo, orientado a objetos, dirigido por eventos, funcional, genérico y reflexivo)",
+            website = "https://docs.microsoft.com/en-us/dotnet/visual-basic/",
+            photoLogo = "https://th.bing.com/th/id/OIP.KY02Krw4ZK87mEVjRHnwsAAAAA?pid=ImgDet&rs=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Perl",
+            author = "Larry Wall",
+            yearCreation = 1987,
+            paradigm = "Multiparadigma (imperativo, orientado a objetos (basado en clases), eventos, procedural, funcional, genérico y reflexivo)",
+            website = "https://www.perl.org/",
+            photoLogo = "https://th.bing.com/th/id/R.f321178e3a3ff064526b4356a30bf6c1?rik=n5FNpAFdQYMUFQ&riu=http%3a%2f%2fwww.ecured.cu%2fimages%2f2%2f2a%2fPerl_logo.jpg&ehk=RcLr5yWa792c%2ftR%2bzQO2r7fly%2by7RXczYfRLV9yjMNg%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Lua",
+            author = "Roberto Ierusalimschy, Luis Enrique de Figueiredo y Waldemar Celes",
+            yearCreation = 1993,
+            paradigm = "Multiparadigma (interpretado, imperativo (procedural, basado en prototipos, orientado a objetos) y funcional)",
+            website = "https://www.lua.org/",
+            photoLogo = "https://th.bing.com/th/id/R.2bc24ca727980663dd8ced88ca88270c?rik=WN%2bmK14FbRPAMg&riu=http%3a%2f%2fwww.solocodigoweb.com%2fwp-content%2fuploads%2f2014%2f03%2flua.jpg&ehk=5LdY7Pj%2bCbm3ns6UmAy3PO8%2f3UnlWdAypUGYTFzFMHk%3d&risl=&pid=ImgRaw&r=0"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "COBOL",
+            author = "CODASYL y Departamento de defensa de EEUU",
+            yearCreation = 1959,
+            paradigm = "Programación estructural, dinámica y orientada a objetos",
+            website = "https://es.wikipedia.org/wiki/COBOL",
+            photoLogo = "https://s3.amazonaws.com/s3.timetoast.com/public/uploads/photo/14760039/image/e19341cdd1a5a5d78667996199d83ba7"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Julia",
+            author = "Jeff Bezanson, Stefan Karpinski, Viral B. Shah y Alan Edelman",
+            yearCreation = 2012,
+            paradigm = "Multiparadigma (multiple dispatch (Programación orientada a objetos), Programación por procedimientos, functional y meta)",
+            website = "https://julialang.org/",
+            photoLogo = "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2014/02/julia-logo-540x305.png"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Erlang",
+            author = "Joe Armstrong",
+            yearCreation = 1986,
+            paradigm = "Porgramación funcional y orientado a la concurrencia",
+            website = "https://www.erlang.org/",
+            photoLogo = "https://th.bing.com/th/id/R.b0e9e57313800660e2dce4ae77d325f2?rik=Exxz2alje66Ijg&pid=ImgRaw&r=0&sres=1&sresct=1"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Groovy",
+            author = "Java Community Process y Apache Software Foundation",
+            yearCreation = 2003,
+            paradigm = "Porgramación orientado a objetos y funcional",
+            website = "http://groovy-lang.org/",
+            photoLogo = "https://everipedia-storage.s3.amazonaws.com/ProfilePicture/en/Groovy__u_28programming_language_u_29__b259e8/Groovy-logo.svg__24024__thumb.png?nocache=795eeb"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Miranda",
+            author = "David Turner",
+            yearCreation = 1985,
+            paradigm = "Porgramación funcional",
+            website = "https://www.cs.kent.ac.uk/people/staff/dat/miranda/",
+            photoLogo = "https://th.bing.com/th/id/R.645560e35226b95a579b91e26fa0e77c?rik=3uIJgaJQRIvBVg&riu=http%3a%2f%2fprogopedia.ru%2fstatic%2fupload_img%2f2011%2f05%2f13%2fMiranda_logo.jpg&ehk=wT5Lv%2f4cnhX8SeTrrl2DW%2feLRMxTcJP91v7z2Ai%2fG2g%3d&risl=&pid=ImgRaw&r=0"
+        ),
+        ProgrammingLanguage(
+            nameLanguage = "Pascal",
+            author = "Niklaus Wirth",
+            yearCreation = 1970,
+            paradigm = "Imperativo (procedural) y estructurado",
+            website = "https://es.wikipedia.org/wiki/Pascal_(lenguaje_de_programaci%C3%B3n)",
+            photoLogo = "https://th.bing.com/th/id/OIP.4JL2gh0eomjmo9JSFQ26BgHaHa?pid=ImgDet&rs=1"
+        )
+    )
+}
