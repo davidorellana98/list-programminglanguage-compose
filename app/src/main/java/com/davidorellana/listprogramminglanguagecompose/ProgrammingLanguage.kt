ffe61af82abe4abb258c4c7cc1b305a8476e4bf8
davidorellana <davidorellana_98@hotmail.com>
2022-01-23 19:13:34 -0500

    Creación Propiedades Lenguaje de Programación

---
+++ app/src/main/java/com/davidorellana/listprogramminglanguagecompose/ProgrammingLanguage.kt
@@ -0,0 +1,10 @@
+package com.davidorellana.listprogramminglanguagecompose
+
+data class ProgrammingLanguage(
+    val nameLanguage: String,
+    val author: String,
+    val yearCreation: Int,
+    val paradigm: String,
+    val website: String,
+    val photoLogo: String
+)
